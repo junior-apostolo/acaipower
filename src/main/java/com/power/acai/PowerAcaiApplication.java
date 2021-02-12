c6e07dde4ee9a12addbfb935f343853d2cee05b0
junior-apostolo <57808229+junior-apostolo@users.noreply.github.com>
2021-02-11 22:04:37 -0300

    first commit

---
+++ src/main/java/com/power/acai/PowerAcaiApplication.java
@@ -0,0 +1,13 @@
+package com.power.acai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerAcaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerAcaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/power/acai/PowerAcaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.acai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerAcaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
